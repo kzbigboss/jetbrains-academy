c17f916844821c5cfe9d6b832ec0a4dfaa43b348
Mark Kazzaz <markkazzaz@gmail.com>
2020-08-23 16:34:31 -0700

    completed stage four

--- java/Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ java/Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,35 +4,113 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        // Capture inputs
+        // initial variables
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int availableWater = scanner.nextInt();
+        int amount_water = 400;
+        int amount_milk = 540;
+        int amount_coffee = 120;
+        int amount_cups = 9;
+        int amount_money = 550;
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int availableMilk = scanner.nextInt();
+        // print status
+        printStatus(amount_water, amount_milk, amount_coffee, amount_cups, amount_money);
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int availableCoffee = scanner.nextInt();
+        // determine valid transaction
+        String transaction;
+        do {
+            System.out.println("Write action (buy, fill, take):");
+            transaction = scanner.nextLine();
+        } while (!validInput(transaction));
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int necessaryCups = scanner.nextInt();
+        // perform transaction
+        switch (transaction) {
+            case "buy":
+                // determine valid item to buy
+                int choice;
+                do {
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                    choice = scanner.nextInt();
+                } while (!validBuy(choice));
 
-        // Calculate cups that can be made from each ingredient
-        double cupsWater = (double) availableWater / 200;
-        double cupsMilk = (double) availableMilk / 50;
-        double cupsCoffee = (double) availableCoffee / 15;
-        int availableCups = (int) Math.min(cupsWater, Math.min(cupsCoffee, cupsMilk));
+                // transact drink
+                switch (choice) {
+                    case 1:
+                        amount_water -= 250;
+                        amount_coffee -= 16;
+                        amount_cups -= 1;
+                        amount_money += 4;
+                        break;
+                    case 2:
+                        amount_water -= 350;
+                        amount_milk -= 75;
+                        amount_coffee -= 20;
+                        amount_cups -= 1;
+                        amount_money += 7;
+                        break;
+                    case 3:
+                        amount_water -= 200;
+                        amount_milk -= 100;
+                        amount_coffee -= 12;
+                        amount_cups -= 1;
+                        amount_money += 6;
+                        break;
+                }
 
-        // Report out results
-        if (availableCups < necessaryCups) {
-            System.out.println("No, I can only make " + availableCups + " cup(s) of coffee");
-        } else if (availableCups > necessaryCups) {
-            int deltaCups = availableCups - necessaryCups;
-            System.out.println("Yes, I can make that amount of coffee (and even " + deltaCups + " more than that)");
-        } else {
-            System.out.println("Yes, I can make that amount of coffee");
+                printStatus(amount_water, amount_milk, amount_coffee, amount_cups, amount_money);
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                amount_water += scanner.nextInt();
+
+                System.out.println("Write how many ml of milk do you want to add:");
+                amount_milk += scanner.nextInt();
+
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                amount_coffee += scanner.nextInt();
+
+                System.out.println("Write how many disposable cups of coffee do you want to add:");
+                amount_cups += scanner.nextInt();
+
+                printStatus(amount_water, amount_milk, amount_coffee, amount_cups, amount_money);
+                break;
+            case "take":
+                System.out.println("I gave you $" + amount_money);
+                System.out.println();
+                amount_money = 0;
+
+                printStatus(amount_water, amount_milk, amount_coffee, amount_cups, amount_money);
+                break;
         }
+    }
+
+    public static void printStatus(int water, int milk, int coffee, int cups, int money) {
+        System.out.println("The coffee machine has:");
+        System.out.println(water + " of water");
+        System.out.println(milk + " of milk");
+        System.out.println(coffee + " of coffee beans");
+        System.out.println(cups + " of disposable cups");
+        System.out.println(money + " of money");
+        System.out.println();
+    }
+
+    public static boolean validInput(String input) {
+        boolean result = false;
+
+        if (input.equals("buy") || input.equals("fill") || input.equals("take")) {
+            result = true;
+        }
+        return result;
+
+    }
+
+    public static boolean validBuy(int input) {
+        boolean result = false;
+
+        if (input > 0 && input <= 3) {
+            result = true;
+        }
+
+        return result;
     }
 }
